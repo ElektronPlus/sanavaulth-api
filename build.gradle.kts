2215f5fcfaacd84708ad085f695b64cd96833775
wybran <krystian.wybranowski@gmail.com>
2022-11-16 18:47:43 +0100

    repair jar build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    application
     id("org.springframework.boot") version "2.7.5"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
@@ -46,4 +47,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("app.sanavaulth.SanavaulthApplicationKt")
 }
