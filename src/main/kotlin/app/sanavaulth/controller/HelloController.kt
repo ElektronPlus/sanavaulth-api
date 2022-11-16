01eb24a96db76cd73d4f0e8b05cfca9536be3d8f
wybran <krystian.wybranowski@gmail.com>
2022-11-16 18:18:05 +0100

    hello page

---
+++ src/main/kotlin/app/sanavaulth/controller/HelloController.kt
@@ -0,0 +1,12 @@
+package app.sanavaulth.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    fun hello() = "sanavaulth"
+
+}
